2b9bc9c476f571687317641a48dbd11724a4ca12
projjal1 <gopprojjal@gmail.com>
2020-11-15 13:53:24 +0530

    Minor changes to HTML and added heroku functionality.

--- character/settings.py
+++ character/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static/')]
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
